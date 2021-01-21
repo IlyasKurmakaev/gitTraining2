96acf94b0f00cbd3c70d307db136a1ad6e1b0b81
IlyasKurmakaev <ilyaskurmakaev@gmail.com>
2021-01-21 15:43:06 +0300

    branch1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("textMaster2");//особый коммит
+        System.out.println("branch1");
         System.out.println("branch2");
     }
 }
